ea87db90dd458b72de90e1b67fd03560b75e9f21
Dustin Davis and Josh Overly <joshoverly@yahoo.com>
2016-01-21 15:48:48 -0800

    Will examine code that we will study

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,38 @@
-var romnum = function(number) {
-  var number = "1";
-  var converted = "I";
+// var romnum = function(number) {
+//   var  = "1";
+//   var converted = "I";
+//
+//   return converted;
+// };
 
-  return converted;
+
+
+
+
+function toRoman(n) {
+  var decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+  for (var i = 0; i < decimals.length; i++) {
+      if(n < 1)
+          return "";
+
+      if(n >= decimals[i]) {
+          var conversion = roman[i] + toRoman(n - decimals[i]);
+          return conversion;
+      }
+  }
 };
+
+$(function() {
+  $("form#counter").submit(function(event) {
+    var n = parseInt($("input#count-to").val());
+    var numbers = toRoman(n);
+
+    $(".output").text(numbers);
+
+    $("#result").show();
+    event.preventDefault();
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,20 @@
-describe('romnum', function() {
-  it("will conver '1' to 'I'", function() {
-    expect(romnum("1")).to.equal("I");
+// describe('romnum', function() {
+//   it("will convert '1' to 'I'", function() {
+//     expect(romnum("1")).to.equal("I");
+//   });
+//   it("will convert '5' to 'V'", function() {
+//     expect(romnum("5")).to.equal("V");
+//   });
+// });
+
+describe('toRoman', function() {
+  it("will convert '5' to 'V'", function() {
+    expect(toRoman("5")).to.equal("V");
+  });
+  it("will convert '10' to 'X'", function() {
+    expect(toRoman("10")).to.equal("X");
+  });
+  it("will convert '14' to 'XIV'", function() {
+    expect(toRoman("14")).to.equal("XIV");
   });
 });
